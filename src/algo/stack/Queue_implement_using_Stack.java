1b5aa60d65eb6f84dc53f1861ce33b94febe0656
poojanitp <pujakumari2805@gmail.com>
2019-09-24 18:58:21 +0530

    queue implementation using stack

---
+++ src/algo/stack/Queue_implement_using_Stack.java
@@ -0,0 +1,26 @@
+package algo.stack;
+import algo.queue.*;
+import algo.stack.stack;
+public class Queue_implement_using_Stack implements Queue,stack{
+
+    public boolean isEmpty(){
+
+
+    }
+    public boolean isFull(){
+
+    }
+    public void enqueue(int data){
+
+    }
+    public int dequeue(){
+
+    }
+    public void push(int item){
+
+    }
+    public int pop(){
+
+    }
+
+}
