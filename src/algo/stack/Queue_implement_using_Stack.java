58b58ca540143a5fc7c37a1542276036abc55020
poojanitp <pujakumari2805@gmail.com>
2019-09-24 19:13:59 +0530

    second commit

--- src/algo/stack/Queue_implement_using_Stack.java
+++ src/algo/stack/Queue_implement_using_Stack.java
@@ -2,6 +2,13 @@ package algo.stack;
 import algo.queue.*;
 import algo.stack.stack;
 public class Queue_implement_using_Stack implements Queue,stack{
+    int capacity,size;
+    int array[];
+    public Queue_implement_using_Stack(int capacity){
+        this.capacity=capacity;
+        this.array=new int[ this.capacity];
+        this.size=0;
+    }
 
     public boolean isEmpty(){
 
@@ -11,10 +18,20 @@ public class Queue_implement_using_Stack implements Queue,stack{
 
     }
     public void enqueue(int data){
-
+       this.push(data);
     }
     public int dequeue(){
 
+        if(this.isEmpty())
+             return -111111111;
+        else {
+            int x = this.pop();
+            if (this.isEmpty()) {
+                return x;
+            } else {
+
+            }
+        }
     }
     public void push(int item){
 
